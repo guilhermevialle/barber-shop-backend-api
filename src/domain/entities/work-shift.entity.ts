09cc95a6977ad2233116a16f3a3f7f5ea69566e1
guilhermevialle <vialleguilherme@gmail.com>
2025-06-05 19:54:34 -0300

    added validation in workshift

--- src/domain/entities/work-shift.entity.ts
+++ src/domain/entities/work-shift.entity.ts
@@ -1,3 +1,4 @@
+import { InvalidTimeError } from "../errors/shared-errors";
 import { idGeneratorService } from "../services/id-generator.service";
 import {
   RequiredWorkShiftProps,
@@ -15,6 +16,23 @@ export class WorkShift {
     };
 
     workShiftSchema.parse(this.props);
+    this.validate();
+  }
+
+  // private methods
+  private validate() {
+    if (this.props.endTime.isBefore(this.props.startTime))
+      throw new InvalidTimeError("End time must be after start time");
+
+    if (this.props.endTime.isEqual(this.props.startTime))
+      throw new InvalidTimeError("End time must be different from start time");
+
+    if (
+      this.props.endTime.subMinutes(this.props.startTime.minutes).minutes < 30
+    )
+      throw new InvalidTimeError(
+        "End time must be at least 30 minutes after start time"
+      );
   }
 
   // static methods
